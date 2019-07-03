16ac92d8fd1bb80bb16cc7332f1dd9cdeb26a01f
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-03 16:07:28 +0700

    add file exercise 12 to directory master

---
+++ exercise-12.js
@@ -0,0 +1,31 @@
+// Diberikan sebuah function konversiMenit(menit) 
+// yang menerima satu parameter berupa angka yang 
+// merupakan ukuran waktu dalam menit. Function akan 
+// me-return string waktu dalam format jam:menit 
+// berdasarkan menit tersebut. 
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit){
+    // menentukan menit berapa
+      var minute  = menit % 60;
+      
+    // menentukan jam berapa
+      var hour    = Math.floor(menit / 60);
+      
+    // meletakan output waktu
+      var time    = '';
+    
+    // pengondisian supaya menit tetap 2 karakter
+      if(minute >= 0 && minute <= 9){
+        time = hour + ':0' + minute;
+      } else {
+        time = hour + ':' + minute;
+      }
+    return time;
+  }
+  
+  console.log(konversiMenit(63)); // 1:03
+  console.log(konversiMenit(124)); // 2:04
+  console.log(konversiMenit(53)); // 0:53
+  console.log(konversiMenit(88)); // 1:28
+  console.log(konversiMenit(120)); // 2:00
