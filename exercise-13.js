3b4f7c637ff24bacedd914cb6d537875bc3ee4c9
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-03 21:53:21 +0700

    add comentar in top code

--- exercise-13.js
+++ exercise-13.js
@@ -1,3 +1,8 @@
+// Diberikan sebuah function xo(str) 
+// yang menerima satu parameter berupa string. 
+// Function akan me-return true jika jumlah 
+// karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
 function xo(str){
     var countX = 0;
     var countO = 0;
